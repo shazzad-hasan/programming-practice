b9f4fd1165fbaa1b12fc4f1ca12e3e64700ba670
Shazzad Hasan <shazzadraihan@gmail.com>
2022-03-22 14:15:12 +0600

    added DNA_translation

---
+++ DNA_translation/dna_translation.py
@@ -0,0 +1,16 @@
+"""
+In this project, we I will translate sequence of DNA into sequence of Amino acids.
+Example:
+    DNA sequence: ATACAATGGCAA
+    Amino acids sequence: IQWQ
+
+We will first download a DNA sequence, translate the DNA sequence into amino acids and then
+download amino acid sequence to check our solution. 
+
+Tasks:
+    1. Manually download DNA and protein sequence data
+    2. Import the DNA data into Python
+    3. Create an algorithm to translate the DNA
+    4. Check if translation matches downloaded amino acid sequence
+
+"""
